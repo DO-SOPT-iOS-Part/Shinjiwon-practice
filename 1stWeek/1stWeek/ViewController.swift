25ee7728d68668d3893095843f8d64ad7cf61101
shinzz410 <tlswldnjs410@gmail.com>
2023-10-07 15:12:55 +0900

    초기세팅

---
+++ 1stWeek/1stWeek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  1stWeek
+//
+//  Created by 신지원 on 2023/10/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
