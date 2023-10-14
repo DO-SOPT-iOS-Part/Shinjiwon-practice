dbcc3a70acff116a70e9edcb11bba72aba2d4629
shinzz410 <tlswldnjs410@gmail.com>
2023-10-14 14:26:28 +0900

    프로젝트 생성

---
+++ 2ndWeek/2ndWeek/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2ndWeek
+//
+//  Created by 신지원 on 2023/10/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
